db846b329f51553e2ce708cbccd3690bd8f9c6f3
hojoon018 <skewed018@gmail.com>
2024-03-14 11:15:05 +0900

    UnityProject_2024_2_B

---
+++ UnityProject_2024_2_B/Assets/Scripts/GameData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New GameData" , menuName = "GameData" , order = 50)]
+
+public class GameData : ScriptableObject
+
+{
+    public string gameName;
+    public int gameScore;
+    public bool isGameActive;
+}
